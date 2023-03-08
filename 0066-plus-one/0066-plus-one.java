c33c2b2e258ebc18a127ead63699f9f42828d635
Vinay Rathi <72222842+vinayy-8878@users.noreply.github.com>
2023-03-09 01:14:39 +0530

    Time: 0 ms (100.00%), Space: 41 MB (38.82%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -2,23 +2,23 @@ class Solution {
     public int[] plusOne(int[] digits) {
          ArrayList<Integer> list=new ArrayList<Integer>();
      
-        int k=1;
+         int k=1;
         int count=0;
         for(int i=digits.length-1;i>=0;i--){
             list.add((digits[i]+k)%10);
             count++;
-            k=(digits[i]+k)/10 ;
+            k=(digits[i]+k)/10;
         }
-        while(k>0){
-        list.add(k%10);
-             count++;
+        while(k!=0){
+            list.add(k%10);
+            count++;
             k=k/10;
         }
-       // Collections.reverse(list);
-           int[] arr=new int[count];
-        for(int i=0;i<count;i++){
-            arr[count-1-i]=list.get(i);
+        int[] arr=new int[count];
+        for(int j=0;j<count;j++){
+            arr[count-1-j]=list.get(j);
         }
+      
         return arr;
     }
 }
