d951009e11d55f10b828360dba17ce73ed17c6ed
Vinay Rathi <72222842+vinayy-8878@users.noreply.github.com>
2023-03-11 23:03:32 +0530

    Time: 0 ms (100.00%), Space: 39.9 MB (17.28%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.java
+++ 0461-hamming-distance/0461-hamming-distance.java
@@ -1,8 +1,8 @@
 class Solution {
     public int hammingDistance(int x, int y) {
-        int n=x>y?x:y;
+      //  int n=x>y?x:y;
         int count=0;
-        while(n!=0){
+        while(x!=0 || y!=0){
          int x1=x & 1;
          int y1=y & 1;
          if(x1!=y1)  { 
@@ -10,7 +10,7 @@ class Solution {
                      }
          x=x>>1;
          y=y>>1;
-            n=x>y?x:y;
+           // n=x>y?x:y;
             
         }
         return count;
