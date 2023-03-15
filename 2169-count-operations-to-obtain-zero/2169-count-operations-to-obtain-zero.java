0b63c4f2ba1584a4a844e78fbed08a574846383b
Vinay Rathi <72222842+vinayy-8878@users.noreply.github.com>
2023-03-15 19:46:42 +0530

    Time: 15 ms (6.07%), Space: 47 MB (5.86%) - LeetHub

--- 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.java
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.java
@@ -1,18 +1,18 @@
 class Solution {
     public int countOperations(int num1, int num2) {
-        int count=0;
-        while(num1!=0 && num2!=0 ){
-        if(num1>=num2){
-        num1=num1-num2;
-            count++;
-        }
-        else
-        {
-       num2=num2-num1;
-            count++;
-        }
-        }
-        
+      int ans=function(num1,num2);
+        return ans;
+    }
+     public int function(int num1,int num2){
+      return helper(num1,num2,0);
+    }
+    public int helper(int num1,int num2,int count){
+    if(num1==0 || num2==0){
         return count;
+     }
+        if(num1>=num2){
+        return helper(num1-num2,num2,count+1);
+        }
+       return helper(num1,num2-num1,count+1);
     }
 }
