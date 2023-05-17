df92b6816975446c2d8a94c233794cc6f4bf457f
Vinay Rathi <72222842+vinayy-8878@users.noreply.github.com>
2023-05-17 13:02:37 +0530

    Time: 16 ms (5.15%), Space: 61.1 MB (8.23%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,25 +1,35 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        k=k%nums.length;
-        System.out.println(k);
-        if(nums.length>1){
-      int[] arr=new int[nums.length];
+        int[] arr= new int[nums.length];
+       k=k%nums.length;
         for(int i=0;i<nums.length;i++){
-         arr[i]=nums[nums.length-1-i];
+            arr[(i+k)%nums.length]=nums[i];
         }
-        int[] ans=new int[nums.length];
-        for(int i=0;i<k;i++){
-        ans[i]=arr[k-i-1];
-        }
-        int j=0;
-        for(int i=k;i<nums.length;i++){
-        ans[nums.length-j-1]=arr[i];
-            j++;
-        }
-        for(int i=0;i<nums.length;i++){
-         nums[i]=ans[i];
-        }
-        }
-       // System.out.println(Arrays.toString(ans));
+         for(int i=0;i<nums.length;i++){
+        nums[i]=arr[i];
+         }
+        System.out.println(Arrays.toString(nums));
+        
+      //   k=k%nums.length;
+      //   System.out.println(k);
+      //   if(nums.length>1){
+      // int[] arr=new int[nums.length];
+      //   for(int i=0;i<nums.length;i++){
+      //    arr[i]=nums[nums.length-1-i];
+      //   }
+      //   int[] ans=new int[nums.length];
+      //   for(int i=0;i<k;i++){
+      //   ans[i]=arr[k-i-1];
+      //   }
+      //   int j=0;
+      //   for(int i=k;i<nums.length;i++){
+      //   ans[nums.length-j-1]=arr[i];
+      //       j++;
+      //   }
+      //   for(int i=0;i<nums.length;i++){
+      //    nums[i]=ans[i];
+      //   }
+      //   }
+      //  // System.out.println(Arrays.toString(ans));
     }
 }
