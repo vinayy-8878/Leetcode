3b1ba8253fdf0dfd71f7054870c105bb427bc386
Vinay Rathi <72222842+vinayy-8878@users.noreply.github.com>
2024-03-20 15:55:00 +0530

    Time: 1091 ms (5.08%), Space: 47.3 MB (6.49%) - LeetHub

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.java
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.java
@@ -1,17 +1,19 @@
+import java.math.BigInteger;
 class Solution {
     public List<Integer> addToArrayForm(int[] num, int k) {
-         ArrayList<Integer> list = new ArrayList<>();
-        int i = num.length - 1;
-        int carry = k;
-        
-        while (i >= 0 || carry > 0) {
-            if (i >= 0)
-                carry += num[i];
-            list.add(0, carry % 10);
-            carry /= 10;
-            i--;
+         ArrayList<Integer> list = new ArrayList<Integer>();
+        BigInteger sum =BigInteger.ZERO;;
+          for (int i = 0; i < num.length; i++) {
+            sum = BigInteger.valueOf(num[i]).add(sum.multiply(BigInteger.TEN));
         }
+        BigInteger n = sum.add(BigInteger.valueOf(k));
+        int rem = 0;
         
+         while (!n.equals(BigInteger.ZERO)) {
+            rem = n.mod(BigInteger.TEN).intValue();
+            list.add(0, rem);
+            n = n.divide(BigInteger.TEN);
+        }
         return list;
     }
 }
