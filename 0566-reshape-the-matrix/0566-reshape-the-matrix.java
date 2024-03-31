5ba19a8bb598e80eb15401ef09b7fd65f3c8b592
Vinay Rathi <72222842+vinayy-8878@users.noreply.github.com>
2024-03-31 18:52:55 +0530

    Time: 1 ms (75.33%), Space: 45.4 MB (26.25%) - LeetHub

---
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        if((r*c) !=(mat.length*mat[0].length)){
+            return mat;
+        }
+    int[][] ans=new int[r][c];
+        int row=0;
+        int col=0;
+        for(int i=0;i<mat.length;i++){
+            for(int j=0;j<mat[0].length;j++){
+                ans[row][col]=mat[i][j];
+                col++;
+                if(col==c){
+                    row++;
+                    col=0;
+                }
+            }
+        }
+    return ans;
+    }
+}
