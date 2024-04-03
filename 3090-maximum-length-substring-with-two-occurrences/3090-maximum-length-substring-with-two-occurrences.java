c3fc3f9fc530e344aca36832e5b3f2ddda01f31b
Vinay Rathi <72222842+vinayy-8878@users.noreply.github.com>
2024-04-03 16:04:38 +0530

    Time: 2 ms (73.82%), Space: 42.3 MB (69.82%) - LeetHub

--- 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.java
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.java
@@ -3,13 +3,17 @@ class Solution {
         
         int n=s.length();
         int maxLength=0;
-        for(int i=0;i<n;i++){
             int[] arr=new int[26];
+        for(int i=0;i<n;i++){
+        
            for(int j=i;j<n;j++){
                arr[s.charAt(j) - 'a']++; 
                if(arr[s.charAt(j)-'a']==3) break;
               maxLength = Math.max(maxLength, j - i + 1);
            }
+            for (int k = 0; k < arr.length; k++) {
+    arr[k] = 0;
+}
         }
         return maxLength;
     }
